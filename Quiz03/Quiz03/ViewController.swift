3a326332710c6d187ae5250196da6669de79b63d
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2022-07-23 11:56:59 +0900

    Add function on btnResult

--- Quiz03/Quiz03/ViewController.swift
+++ Quiz03/Quiz03/ViewController.swift
@@ -18,10 +18,25 @@ class ViewController: UIViewController {
     }
 
     @IBAction func btnResult(_ sender: UIButton) {
-        if Int(tfNum.text) % 2 == 0 {
-            lblResult.text = "입력하신 숫자는 짝수입니다."
+        self.view.endEditing(true)
+
+        if tfNum.text!.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+            tfNum.becomeFirstResponder()
+            lblResult.text = "숫자를 확인하세요!"
+        }
+        else {
+            let num = Int(tfNum.text!)!
+
+            if num % 2 == 0 {
+                lblResult.text = "입력하신 숫자는 짝수입니다."
+            } else{
+                lblResult.text = "입력하신 숫자는 홀수입니다."
+            }
         }
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
 
