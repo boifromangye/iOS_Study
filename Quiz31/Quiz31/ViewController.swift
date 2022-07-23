c9bc848be74f5faa19bccdc6e1fc0a590a8acb2a
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2022-07-23 14:07:12 +0900

    Create Quiz31

---
+++ Quiz31/Quiz31/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Quiz31
+//
+//  Created by 권태우 on 2022/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var tfNum1: UITextField!
+    @IBOutlet weak var tfNum2: UITextField!
+    @IBOutlet weak var tfResult: UITextField!
+    @IBOutlet weak var lblMsg: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnAdd(_ sender: UIButton) {
+        
+    }
+    
+    func
+    
+}
+
