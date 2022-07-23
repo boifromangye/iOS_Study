8dea38a5a1d83cbba3fafda065efdbf571da2fa5
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2022-07-23 14:20:28 +0900

    Add functions

--- Quiz31/Quiz31/ViewController.swift
+++ Quiz31/Quiz31/ViewController.swift
@@ -16,13 +16,39 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        lblMsg.text = ""
     }
 
     @IBAction func btnAdd(_ sender: UIButton) {
+        self.view.endEditing(true)
+
+        guard let num1 = tfNum1.text else {return}
+        guard let num2 = tfNum2.text else {return}
         
+        if isNil(str: num1) {
+            lblMsg.text = "1번 숫자를 확인하세요!"
+            tfNum1.becomeFirstResponder()
+        } else{
+            if isNil(str: num2) {
+                lblMsg.text = "2번 숫자를 확인하세요!"
+                tfNum2.becomeFirstResponder()
+            }else{
+                tfResult.text = addition(str1: num1, str2: num2)
+                lblMsg.text = "계산이 완료되었습니다."
+            }
+        }
     }
     
-    func
+    func isNil(str : String) -> Bool{
+        if str.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+            return true
+        } else{
+            return false
+        }
+    }
     
+    func addition(str1:String, str2:String) -> String{
+        return String(Int(str1)! + Int(str2)!)
+    }
 }
 
