99e83b027029d63b786da8de3c5d38276cea167d
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2022-07-23 16:12:07 +0900

    Add functions

--- Quiz32/Quiz32/ViewController.swift
+++ Quiz32/Quiz32/ViewController.swift
@@ -8,12 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tfNumStart: UITextField!
+    @IBOutlet weak var tfNumEnd: UITextField!
+    @IBOutlet weak var lblResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        lblResult.text?.removeAll()
     }
 
+    @IBAction func btnAdd(_ sender: UIButton) {
+        guard var startNum = tfNumStart.text else { return }
+        guard var endNum = tfNumEnd.text else { return }
+        var a : String
 
+        startNum = isNil(str: startNum)
+        endNum = isNil(str: endNum)
+        if !compareNum(start: startNum, end: endNum){
+            a = startNum
+            startNum = endNum
+            endNum = a
+        }
+        lblResult.text = "결과 : \(rangeAddition(start: startNum, end: endNum))"
+    }
+    
+    func isNil(str:String) -> String{
+        if str.isEmpty {
+            return "0"
+        }else{
+            return str
+        }
+    }
+    
+    func compareNum(start:String, end:String) -> Bool{
+        let num1 = Int(start)!
+        let num2 = Int(end)!
+        
+        if num1 < num2 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func rangeAddition(start:String, end:String) -> Int{
+        let num1 = Int(start)!
+        let num2 = Int(end)!
+        var sum = 0
+        
+        for i in num1...num2{
+            sum += i
+        }
+        
+        return sum
+    }
 }
 
