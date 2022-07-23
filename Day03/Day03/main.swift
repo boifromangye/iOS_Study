e1cb54de97c16948dc8da22bae99c04092db006d
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2022-07-23 11:24:27 +0900

    Add Comparison Operator

--- Day03/Day03/main.swift
+++ Day03/Day03/main.swift
@@ -9,3 +9,16 @@ import Foundation
 
 // 비교 연산자
 print(1 == 1)
+print(2 != 1)
+print(2 > 1)
+print(1 < 2)
+print(1 >= 1)
+print(2 <= 1)
+
+let name = "world"
+
+if name == "world"{
+    print("Hello \(name)!")
+} else{
+    print("I'm sorry, \(name).")
+}
