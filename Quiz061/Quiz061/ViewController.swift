9b8bf8c3ad66e867efa9d24ef453d9ef8fd03d98
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2022-07-24 15:45:57 +0900

    Create Quiz061

---
+++ Quiz061/Quiz061/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Quiz061
+//
+//  Created by 권태우 on 2022/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var imgFlower: UIImageView!
+    @IBOutlet weak var imgPreview: UIImageView!
+    @IBOutlet weak var lblFileName: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnPrev(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnNext(_ sender: UIButton) {
+    }
+}
+
