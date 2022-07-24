4d2dfc2238141c1e9c42246dde6bbd8e9449c483
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2022-07-24 11:19:33 +0900

    Remove tfName

--- Quiz01/Quiz01/ViewController.swift
+++ Quiz01/Quiz01/ViewController.swift
@@ -9,7 +9,6 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var lblMsg: UILabel!
-    @IBOutlet weak var tfName: UITextField!
     var cnt = 0
     
     override func viewDidLoad() {
@@ -18,12 +17,11 @@ class ViewController: UIViewController {
     }
 
     @IBAction func btnName(_ sender: UIButton) {
-        let name = tfName.text ?? "권태우"
         cnt += 1
         if cnt % 2 == 0 {
             lblMsg.text = "Welcome!"
         } else {
-            lblMsg.text = "Welcome! \(name)"
+            lblMsg.text = "Welcome! 권태우)"
         }
     }
     
