1e85dc42fcf05150ec267b6440f7b9e6e4ed2102
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2022-07-24 14:16:47 +0900

    Emoji DONE

--- Emoji/Emoji/ViewController.swift
+++ Emoji/Emoji/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lblEmoji: UILabel!
+    let arr = ["⭐️", "🇰🇷", "⚽️", "📸", "📖", "❤️"]
+    var cnt = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        lblEmoji.text = arr[0]
     }
+    @IBAction func btnPlay(_ sender: UIButton) {
+        cnt += 1
 
-
+        lblEmoji.text = arr[cnt%arr.count]
+    }
 }
 
