94de930fc326110f2e92b6ed492d79b3a3107511
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2022-07-24 14:39:51 +0900

    Add lblArr

--- Emoji/Emoji/ViewController.swift
+++ Emoji/Emoji/ViewController.swift
@@ -9,12 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var lblEmoji: UILabel!
-    let arr = ["⭐️", "🇰🇷", "⚽️", "📸", "📖", "❤️"]
+    @IBOutlet weak var lblArr: UILabel!
+    let arr = ["⭐️", "🇰🇷", "⚽️", "📸", "📖", "❤️", "😛"]
     var cnt = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        lblArr.text?.removeAll()
+        for i in arr {
+            lblArr.text! += i
+        }
         lblEmoji.text = arr[0]
     }
     @IBAction func btnPlay(_ sender: UIButton) {
