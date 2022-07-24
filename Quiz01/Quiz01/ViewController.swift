edae28ae496956ece422d03fb67667557e565e57
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2022-07-24 11:07:58 +0900

    Add btnName and lblMsg

--- Quiz01/Quiz01/ViewController.swift
+++ Quiz01/Quiz01/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lblMsg: UILabel!
+    var cnt = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnName(_ sender: UIButton) {
+        cnt += 1
+        if cnt % 2 == 0 {
+            lblMsg.text = "Welcome!"
+        } else {
+            lblMsg.text = "Welcome! 권태우"
+        }
+    }
+    
 }
 
