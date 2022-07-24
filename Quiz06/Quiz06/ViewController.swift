7d51452b9ec31db28ceb6db3773401e258185027
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2022-07-24 15:15:58 +0900

    Edit functions on btnPrev and btnNext

--- Quiz06/Quiz06/ViewController.swift
+++ Quiz06/Quiz06/ViewController.swift
@@ -10,19 +10,29 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var lblFileName: UILabel!
     @IBOutlet weak var imgFlower: UIImageView!
+    let imgArr = ["flower_01", "flower_02", "flower_03", "flower_04", "flower_05", "flower_06"]
     var cnt = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        lblFileName.text = imgArr[cnt]
+        imgFlower.image = UIImage(named: imgArr[cnt])
     }
 
     @IBAction func btnPrev(_ sender: UIButton) {
-        
+        if cnt <= 0 {
+            cnt = imgArr.count
+        }
+        cnt -= 1
+        lblFileName.text = imgArr[cnt%imgArr.count]
+        imgFlower.image = UIImage(named: imgArr[cnt%imgArr.count])
     }
     
     @IBAction func btnNext(_ sender: UIButton) {
-        
+        cnt += 1
+        lblFileName.text = imgArr[cnt%imgArr.count]
+        imgFlower.image = UIImage(named: imgArr[cnt%imgArr.count])
     }
 }
 
