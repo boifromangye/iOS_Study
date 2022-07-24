5150d186105ec96c7a37be060157f019ab01d07b
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2022-07-24 15:59:54 +0900

    Add images and functions to check names of files

--- Quiz061/Quiz061/ViewController.swift
+++ Quiz061/Quiz061/ViewController.swift
@@ -11,16 +11,49 @@ class ViewController: UIViewController {
     @IBOutlet weak var imgFlower: UIImageView!
     @IBOutlet weak var imgPreview: UIImageView!
     @IBOutlet weak var lblFileName: UILabel!
+    let imgArr = ["flower_01", "flower_02", "flower_03", "flower_04", "flower_05", "flower_06"]
+    var cnt = 0
+    var cntPre = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        displayImg()
     }
 
     @IBAction func btnPrev(_ sender: UIButton) {
+        checkCnt()
+        cnt -= 1
+        cntPre -= 1
+        displayImg()
     }
     
     @IBAction func btnNext(_ sender: UIButton) {
+        checkCnt()
+        cnt += 1
+        cntPre += 1
+        displayImg()
+    }
+    
+    func displayImg() {
+        lblFileName.text = imgArr[cnt%imgArr.count]
+        imgFlower.image = UIImage(named: imgArr[cnt%imgArr.count])
+        imgPreview.image = UIImage(named: imgArr[cntPre%imgArr.count])
+    }
+    
+    func checkCnt() {
+        if cnt <= 0 {
+            cnt = imgArr.count
+        }
+        if cnt >= imgArr.count {
+            cnt = 0
+        }
+        if cntPre <= 0 {
+            cntPre = imgArr.count
+        }
+        if cntPre >= imgArr.count {
+            cntPre = 0
+        }
     }
 }
 
