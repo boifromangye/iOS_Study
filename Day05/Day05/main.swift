1c973d3f7b75d2f5a5f7537149c538d1168afffc
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2022-07-30 15:09:15 +0900

    Add switch statement with enumerate

--- Day05/Day05/main.swift
+++ Day05/Day05/main.swift
@@ -280,3 +280,26 @@ if !bo {
 
 //print("main 2:", a, b, aa, c)
 print("main 2:", a, b, c)
+
+// 열거형을 입력값으로 받는 switch문
+
+enum School2{
+    case primary, elementary, middle, high, college, university, graduate
+}
+
+let 최종학력: School2 = .university
+
+switch 최종학력{
+case .primary:
+    print("최종학력은 유치원입니다.")
+case .elementary:
+    print("최종학력은 초등학교입니다.")
+case .middle:
+    print("최종학력은 중학교입니다.")
+case .high:
+    print("최종학력은 고등학교입니다.")
+case .college, .university:
+    print("최종학력은 대학(교)입니다.")
+case .graduate:
+    print("최종학력은 대학원입니다.")
+}
