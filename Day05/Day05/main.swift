8f78dbebd88e5459aa4ae63720b02bbaf9fc3a1b
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2022-07-30 11:23:38 +0900

    Add Set

--- Day05/Day05/main.swift
+++ Day05/Day05/main.swift
@@ -33,3 +33,36 @@ print(airports.count, airports)
 
 airports["LHR"] = "LONDON"
 print(airports.count, airports)
+
+// Set
+// 집합 연산에 유용
+// 중복값은 처리하지 않는다
+
+var oddDigits: Set = [1,3,5,7,9]
+var evenDigits: Set = [2,4,6,8,0]
+let singleDigitPrimeNumbers: Set = [2,3,5,7]
+
+oddDigits.insert(1) // 중복값 반영 X
+print(oddDigits.sorted())
+
+// 합집합
+print(oddDigits.union(evenDigits).sorted())
+
+// 교집합
+print(oddDigits.intersection(evenDigits).sorted())
+
+// 차집합
+print(oddDigits.subtracting(singleDigitPrimeNumbers).sorted())
+
+// 여집합
+print(oddDigits.symmetricDifference(singleDigitPrimeNumbers).sorted())
+
+// ---
+let houseAnimals: Set = ["dog", "cat"]
+let farmAnimals: Set = ["cow", "chicken", "Sheep", "dog", "cat"]
+let cityAnimals: Set = ["duck", "rabbit"]
+
+print(houseAnimals.isSubset(of: farmAnimals))
+print(farmAnimals.isSuperset(of: houseAnimals))
+print(farmAnimals.isDisjoint(with: cityAnimals))
+
