8d43b6f81c0657c0a6df5b6cc6ee32590b3e58da
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2022-07-30 10:55:59 +0900

    Add dictionary

--- Day05/Day05/main.swift
+++ Day05/Day05/main.swift
@@ -7,5 +7,29 @@
 
 import Foundation
 
-print("Hello, World!")
+// Dictionary : key와 value로 구성된 컬렉션
+// key가 String 타입이고 value가 Int인 빈 Dictionary 생성
 
+// var scoreDictionary: Dictionary<String, Int> = [String: Int]()
+var scoreDict: [String: Int] = [:]
+scoreDict["Kwon"] = 100
+scoreDict["Park"] = 80
+scoreDict["Lee"] = 75
+
+print(scoreDict)
+print(scoreDict["Kwon"]!)
+
+var airports: [String: String] = ["YYZ": "Toronto Pearson", "DUB": "DUBLIN"]
+print(airports.count, airports)
+
+if airports.isEmpty {
+    print("The airport is empty.")
+} else{
+    print("The airport is not empty.")
+}
+
+airports["LHR"] = "London"
+print(airports.count, airports)
+
+airports["LHR"] = "LONDON"
+print(airports.count, airports)
