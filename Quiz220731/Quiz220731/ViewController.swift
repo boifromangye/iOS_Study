f6829f0e6b1e02a21ae7c1b0b006e77b12e57231
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2022-07-31 16:14:37 +0900

    Create TextFieldSwitchCalc

---
+++ Quiz220731/Quiz220731/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  Quiz220731
+//
+//  Created by 권태우 on 2022/07/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var tfNum1: UITextField!
+    @IBOutlet weak var switchAdd: UISwitch!
+    @IBOutlet weak var switchSub: UISwitch!
+    @IBOutlet weak var switchMul: UISwitch!
+    @IBOutlet weak var switchDiv: UISwitch!
+    @IBOutlet weak var tfNum2: UITextField!
+    @IBOutlet weak var tfAddR: UITextField!
+    @IBOutlet weak var tfSubR: UITextField!
+    @IBOutlet weak var tfMulR: UITextField!
+    @IBOutlet weak var tfDivR: UITextField!
+    var addR = 0
+    var subR = 0
+    var mulR = 0
+    var divR: Double = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnCal(_ sender: UIButton) {
+        self.view.endEditing(true)
+        
+        if tfNum1.text!.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty || tfNum2.text!.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+            tfNum1.becomeFirstResponder() // 커서 위치 조정
+        }
+        else {
+            let num1 : Int = Int(tfNum1.text!)!
+            let num2 : Int = Int(tfNum2.text!)!
+            addR = num1 + num2
+            subR = num1 - num2
+            mulR = num1 * num2
+            
+            tfAddR.text! = "\(addR)"
+            tfSubR.text! = "\(subR)"
+            tfMulR.text! = "\(mulR)"
+            if num2 == 0 {
+                tfDivR.text = "N/A"
+            }
+            else {
+                divR = Double(num1) / Double(num2)
+                tfDivR.text = "\(divR)"
+            }
+        }
+    }
+    
+    @IBAction func btnClear(_ sender: UIButton) {
+        tfNum1.text?.removeAll()
+        tfNum2.text?.removeAll()
+        switchAdd.isOn = true
+        switchSub.isOn = true
+        switchMul.isOn = true
+        switchDiv.isOn = true
+        tfAddR.text?.removeAll()
+        tfSubR.text?.removeAll()
+        tfMulR.text?.removeAll()
+        tfDivR.text?.removeAll()
+    }
+    
+    @IBAction func swithAddR(_ sender: UISwitch) {
+        switch sender.isOn{
+        case true: tfAddR.text = "\(addR)"
+        default: tfAddR.text?.removeAll()
+        }
+    }
+    
+    @IBAction func switchSubR(_ sender: UISwitch) {
+        switch sender.isOn{
+        case true: tfSubR.text = "\(subR)"
+        default: tfSubR.text?.removeAll()
+        }
+    }
+    
+    @IBAction func switchMulR(_ sender: UISwitch) {
+        switch sender.isOn{
+        case true: tfMulR.text = "\(mulR)"
+        default: tfMulR.text?.removeAll()
+        }
+    }
+    
+    @IBAction func switchDivR(_ sender: UISwitch) {
+        switch sender.isOn{
+        case true:
+            if Int(tfNum2.text!)! == 0 {
+                tfDivR.text = "N/A"
+            }else {
+                tfDivR.text = "\(divR)"
+            }
+        default: tfDivR.text?.removeAll()
+        }
+    }
+}
+
