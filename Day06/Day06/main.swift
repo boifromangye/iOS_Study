c9407b7a1784bd44e8f0ff50fbf0e39e4e26bb62
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2022-07-31 12:27:38 +0900

    Add Function

--- Day06/Day06/main.swift
+++ Day06/Day06/main.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-
 /*
 //  사용자로부터 더할 숫자의 개수를 정한 후 숫자를 입력받아 합계 구하기
 
@@ -27,7 +26,6 @@ if let repNum: Int = inpNum{
     print("Input value is wrong!")
 }
  */
-
 // Exercise
 /*
  입력한 자릿수 정수의 합을 구하는 프로그램
@@ -36,7 +34,7 @@ if let repNum: Int = inpNum{
  Sum of digits = 36
  
  */
-
+/*
 print("Enter an integer(0~9) :", terminator: " ")
 var inputNum = Int(readLine()!)!
 var sum = 0
@@ -50,3 +48,39 @@ while inputNum != 0{
 }
 
 print("Sum of digits = \(sum)")
+*/
+// ------------------
+// 함수(function)
+// ------------------
+
+func integerSum(a: Int, b: Int) -> Int{
+    return a+b
+}
+
+func integerSum2(n1: Int, n2: Int){
+    print("\(n1) + \(n2) = \(n1+n2)")
+}
+
+func integerSum3(_ n1: Int,_ n2: Int){
+    print("\(n1) + \(n2) = \(n1+n2)")
+}
+
+print(integerSum(a: 10, b: 20))
+integerSum2(n1: 20, n2: 10)
+integerSum3(11, 22)
+
+func sayHelloWorld(){
+    print("Hello World!")
+}
+
+// Ex
+// sayHelloWorld2라는 함수를 만든다
+// 사용법 sayHelloWorld2(3) <- "Hello World" 3번 출력
+
+func sayHelloWorld2(i: Int){
+    for _ in 1...i{
+        print("Hello World")
+    }
+}
+
+sayHelloWorld2(i: 3)
