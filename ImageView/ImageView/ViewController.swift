9b27a84e45444d37d2d7c4a3d9eb38cab57f4d61
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2022-07-31 15:02:58 +0900

    Add functions

--- ImageView/ImageView/ViewController.swift
+++ ImageView/ImageView/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var btnResize: UIButton!
+    var imgOn: UIImage?
+    var imgOff: UIImage?
+    var isZoom = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imgOn = UIImage(named: "lamp_on.png")
+        imgOff = UIImage(named: "lamp_off.png")
     }
 
-
+    @IBAction func btnResizeImg(_ sender: UIButton) {
+        var w: CGFloat
+        var h: CGFloat
+        
+        if isZoom{
+            w = imgView.frame.width / 2
+            h = imgView.frame.height / 2
+            btnResize.setTitle("Image 확대", for: .normal)
+        }else{
+            w = imgView.frame.width * 2
+            h = imgView.frame.height * 2
+            btnResize.setTitle("Image 축소", for: .normal)
+        }
+        imgView.frame.size = CGSize(width: w, height: h)
+        isZoom = !isZoom
+    }
+    
+    @IBAction func switchImg(_ sender: UISwitch) {
+        switch sender.isOn{
+        case true: imgView.image = imgOn
+        default: imgView.image = imgOff
+        }
+    }
 }
 
