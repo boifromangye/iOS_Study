f3f372e8262c3b79e3727e1702d03307ab96e48a
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2022-07-31 10:48:13 +0900

    Add some examples

--- Day06/Day06/main.swift
+++ Day06/Day06/main.swift
@@ -7,4 +7,46 @@
 
 import Foundation
 
+/*
+//  사용자로부터 더할 숫자의 개수를 정한 후 숫자를 입력받아 합계 구하기
 
+var sumNum = 0
+
+print("몇 개의 숫자를 더할까요? ", terminator: "")
+var inpNum = Int(readLine()!)
+//print("===> \(inpNum!) : ", type(of: inpNum))
+
+if let repNum: Int = inpNum{
+    print("\(repNum)개의 숫자를 입력하세요.")
+    for _ in 1...repNum{
+        let innerCount = Int(readLine()!)
+        sumNum += innerCount!
+    }
+    print("입력한 수들의 합은 \(sumNum)입니다.")
+}else{
+    print("Input value is wrong!")
+}
+ */
+
+// Exercise
+/*
+ 입력한 자릿수 정수의 합을 구하는 프로그램
+ 
+ Enter an integer(0~9) : 12345678
+ Sum of digits = 36
+ 
+ */
+
+print("Enter an integer(0~9) :", terminator: " ")
+var inputNum = Int(readLine()!)!
+var sum = 0
+
+while true{
+    if inputNum == 0{
+        break
+    }
+    sum += inputNum % 10
+    inputNum /= 10
+}
+
+print("Sum of digits = \(sum)")
