005d90f6a4d24c38ad89158b995bb55b409601f4
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2022-08-06 14:32:54 +0900

    Add timer on VC

--- DatePicker/DatePicker/ViewController.swift
+++ DatePicker/DatePicker/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lblCurrentTime: UILabel!
+    @IBOutlet weak var lblPickerTime: UILabel!
+    var interval = 1.0
+    let timeSelector: Selector = #selector(ViewController.updateTime)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
     }
 
-
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        
+    }
+    
+    // Async Task로 1초당 1번씩 구동
+    @objc func updateTime(){
+        let date = NSDate()
+        let formatter = DateFormatter()
+        
+        formatter.locale = Locale(identifier: "ko")
+        formatter.dateFormat = "yyyy-MM-dd EEE a hh:mm" // year-month-date day am/pm hour:minute
+        lblCurrentTime.text = "현재 시각 : \(formatter.string(from: date as Date))"
+    }
 }
 
