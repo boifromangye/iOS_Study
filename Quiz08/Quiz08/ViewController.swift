5c8dd264389169ae2070027cf4640f731d2084ca
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2022-08-06 15:57:08 +0900

    Add infinite loop

--- Quiz08/Quiz08/ViewController.swift
+++ Quiz08/Quiz08/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var lblFileName: UILabel!
+    @IBOutlet weak var imgFlower: UIImageView!
+    let imgArr = ["flower_01", "flower_02", "flower_03", "flower_04", "flower_05", "flower_06"]
+    var interval = 3.0
+    let timeSelector: Selector = #selector(ViewController.updateTime)
+    var cnt = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        displayImg()
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
     }
 
-
+    func displayImg() {
+        lblFileName.text = imgArr[cnt%imgArr.count]
+        imgFlower.image = UIImage(named: imgArr[cnt%imgArr.count])
+    }
+    
+    @objc func updateTime(){
+        _ = NSDate()
+        let formatter = DateFormatter()
+        
+        formatter.locale = Locale(identifier: "ko")
+        formatter.dateFormat = "yyyy-MM-dd EEE a hh:mm:ss"
+        
+        cnt += 1
+        displayImg()
+    }
 }
 
