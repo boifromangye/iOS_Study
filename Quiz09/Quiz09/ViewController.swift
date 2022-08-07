e9615621f5ffe56e6670f66b637e8eaf874aa721
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2022-08-07 13:44:28 +0900

    Create Quiz09

---
+++ Quiz09/Quiz09/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Quiz09
+//
+//  Created by 권태우 on 2022/08/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var pickerDan: UIPickerView!
+    @IBOutlet weak var tvResult: UITextView!
+    let dans: [Int] = [2, 3, 4, 5, 6, 7, 8, 9]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        lblTitle.text = "2단"
+        for i in 1...9{
+            tvResult.text += "2 X \(i) = \(2*i)\n"
+        }
+    }
+
+
+}
+
+extension ViewController: UIPickerViewDataSource{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 8
+    }
+}
+
+extension ViewController: UIPickerViewDelegate{
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        tvResult.text.removeAll()
+        lblTitle.text = "\(dans[row])단"
+        for i in 1...9{
+            tvResult.text += "\(dans[row]) X \(i) = \(dans[row]*i)\n"
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let lblPicker = UILabel()
+        lblPicker.text = "\(dans[row])단"
+        lblPicker.textAlignment = .center
+        
+        return lblPicker
+    }
+}
