b65e7987be2d2a0272d5a6256b0af17c3db4e8a2
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2022-08-07 13:53:16 +0900

    Add pickerView

--- PickerView/PickerView/ViewController.swift
+++ PickerView/PickerView/ViewController.swift
@@ -15,20 +15,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imgBackground.image = UIImage(named: imgArr[0])
     }
     
 }
 
+extension ViewController: UIPickerViewDataSource{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return imgArr.count
+    }
+}
+
 extension ViewController: UIPickerViewDelegate{
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        imgBackground.image = UIImage(named: imgArr[row])
+    }
+    
     // PickerView에 thumbnail 배치(viewForRow)
     func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
         let imageView = UIImageView(image: UIImage(named: imgArr[row]))
         imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 20)
         
         return imageView
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        
     }
 }
