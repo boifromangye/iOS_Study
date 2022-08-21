e77a9822739253ce5d2ad3c3f88847086b62b2ab
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2022-08-21 10:31:34 +0900

    Add data source extension

--- Quiz18/Quiz18/ViewController.swift
+++ Quiz18/Quiz18/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        pickerView.dataSource = self
         pickerView.delegate = self
         myWebView.navigationDelegate = self
         loadWebPage(url: "https://" + arrUrl[0])
