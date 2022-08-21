bf2185aa0f55ce76308582bfa9778781dcd16a82
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2022-08-21 12:26:15 +0900

    Done.

--- PageControl/PageControl/ViewController.swift
+++ PageControl/PageControl/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imgFlower: UIImageView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var lblTitle: UILabel!
+    let flowers = ["flower_01", "flower_02", "flower_03", "flower_04", "flower_05", "flower_06"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        pageControl.numberOfPages = flowers.count
+        pageControl.currentPage = 0
+        pageControl.currentPageIndicatorTintColor = UIColor.red
+        imgFlower.image = UIImage(named: flowers[pageControl.currentPage])
+        lblTitle.text = flowers[pageControl.currentPage]
     }
 
-
-}
+    @IBAction func changePage(_ sender: UIPageControl) {
+        imgFlower.image = UIImage(named: flowers[pageControl.currentPage])
+        lblTitle.text = flowers[pageControl.currentPage]
+    }
+} // ViewController
 
