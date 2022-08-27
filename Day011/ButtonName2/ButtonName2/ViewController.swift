fd9d0ba1bca85c6925cd784d91c723457344dbd3
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2022-08-27 12:20:25 +0900

    ButtonName2 Done

--- Day011/ButtonName2/ButtonName2/ViewController.swift
+++ Day011/ButtonName2/ButtonName2/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lblEmoji: UILabel!
+    var emoji = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        lblEmoji.text = "여기에 Emoji가 출력됩니다."
     }
 
-
-}
+    @IBAction func btnMerong(_ sender: UIButton) {
+        emoji += "🤪"
+        lblEmoji.text! = emoji
+    }
+    
+    @IBAction func btnSmile(_ sender: UIButton) {
+        emoji += "😄"
+        lblEmoji.text! = emoji
+    }
+    
+    @IBAction func btnAnnoying(_ sender: UIButton) {
+        emoji += "😡"
+        lblEmoji.text! = emoji
+    }
+    
+    @IBAction func btnRemove(_ sender: UIButton) {
+        emoji.removeLast()
+        lblEmoji.text! = emoji
+    }
+    @IBAction func btnClear(_ sender: UIButton) {
+        emoji.removeAll()
+        lblEmoji.text = "여기에 Emoji가 출력됩니다."
+    }
+    
+} // ViewController
 
